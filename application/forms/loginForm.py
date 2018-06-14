01b104a9e41e9b2bdf04f0da2d6d16faa3128ea9
charlesgiry <charles.giry.laterriere@gmail.com>
2018-06-14 23:28:48 +0200

    change in form template

--- application/forms/loginForm.py
+++ application/forms/loginForm.py
@@ -1,12 +1,13 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
+from wtforms import TextField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 from application.models.user import User
 
 class LoginForm(Form):
-	email = TextField("Email", validators=[DataRequired()])
-	password = PasswordField("Password", validators=[DataRequired()])
+	email = TextField('Email', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 	
 	def __init__(self, *args, **kwargs):
--- application/forms/registerForm.py
+++ application/forms/registerForm.py
@@ -1,16 +1,18 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
+from wtforms import TextField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 from application.models.user import User
 
 class RegisterForm(Form):
-	email = TextField("Email", validators=[DataRequired(), Email()])
-	password = PasswordField("Password", validators=[DataRequired(), 
+	email = TextField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired(), 
 		EqualTo('confirm', message='Password must match'), 
 		Length(min=8, max=24)]
 	)
 	confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+	
 
 	
 	def __init__(self, *args, **kwargs):
