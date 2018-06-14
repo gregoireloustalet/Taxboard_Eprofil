6a94e24b8aafca30e33eef4cfa0510d04c78b816
charlesgiry <charles.giry.laterriere@gmail.com>
2018-06-15 00:04:25 +0200

    added config required for mail

--- application/config.py
+++ application/config.py
@@ -9,11 +9,20 @@ class base_config(object):
 		DB_USER
 		DB_PASSWORD 
 		DB_BUCKET 
+		
+		MAIL_SERVER
+		MAIL_PORT
+		MAIL_USE_TLS
+		MAIL_USE_SSL
+		MAIL_USERNAME 
+		MAIL_PASSWORD
+		MAIL_DEFAULT_SENDER
+		MAIL_SUPPRESS_SEND
 
 	
 	"""
 	
 class dev_config(base_config):
 	DEVELOPMENT = True
-	
+	DEBUG = True
 	
